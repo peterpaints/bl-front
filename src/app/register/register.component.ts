b36bd327d112de136d73a5a0efd25522bb8dded7
peterpaints <musonyengigi@gmail.com>
2017-11-20 15:21:30 +0300

    [chore] Implement register method via the ApiService

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../apiservice.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+  email: string = '';
+  password: string = '';
+  confirm_password: string ='';
+  message: any = '';
+  error: string = '';
+  access_token: string = '';
+  login_status: boolean;
+
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  public register(email, password, confirm_password) {
+    if (password !== confirm_password) {
+      this.error = 'Passwords don\'t match';
+      alert(this.error);
+    } else {
+      this.api.registerUser(email, password)
+      .subscribe(response =>
+        this.message = response,
+        error => alert(error['message'])
+      );
+    }
+    if (this.message.json()['message'] === 'You registered successfully. Please log in.') {
+      this.router.navigate(['/login']);
+    }
+  }
 }
