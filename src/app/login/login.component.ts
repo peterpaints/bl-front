9b7b0b7cb5174e3d86b4e2d7d14d949e7b8526a5
peterpaints <musonyengigi@gmail.com>
2017-11-20 15:17:08 +0300

    [chore] Implement login method via the ApiService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../apiservice.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+  email: string = '';
+  password: string = '';
+  confirm_password: string ='';
+  access_token: string = '';
+  login_status: boolean;
+
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  public login(email, password) {
+    this.api
+      .logUserIn(email, password)
+      .subscribe(response => {
+        const res = response;
+        if (res.json()['message'] === 'You logged in successfully.') {
+          if ((res.json()['access_token'])) {
+            this.access_token = res.json()['access_token'];
+            localStorage.setItem('access_token', this.access_token);
+            localStorage.setItem('login_status', JSON.stringify(this.login_status = true));
+            localStorage.setItem('current_user', email);
+            this.router.navigate(['/dashboard']);
+          }
+        } else {
+          localStorage.setItem('login_status', JSON.stringify(this.login_status = false));
+          localStorage.setItem('current_user', null);
+          this.router.navigate(['/login']);
+        }
+      },
+        error => {alert(error['message']);
+      });
+  }
 }
