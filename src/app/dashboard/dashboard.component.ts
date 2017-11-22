7aa483a680bf312a9d349d6083ec31b2c3b013c2
peterpaints <musonyengigi@gmail.com>
2017-11-22 11:05:30 +0300

    [feature(s)] Add fetch all buckets, create bucket, delete bucket, searching and pagination functionality

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Bucket } from '../../models/buckets';
+import { ApiService } from '../apiservice.service';
+import { Router } from '@angular/router';
+declare var $: any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +10,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  newBucket: Bucket = new Bucket();
+  bucketlists: Bucket[] = [];
+  current_user: string = '';
+  access_token: string = '';
+  login_status: boolean;
+  prev: number;
+  current_page: number = 1;
+  next: number = this.current_page + 1;
+  per_page: number = 12;
+  search: string = '';
+  temp: number;
+
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit() {
+    if (JSON.parse(localStorage.getItem('login_status')) === false) {
+      this.router.navigate(['/login']);
+    } else {
+      this.current_user = localStorage.getItem('current_user');
+      this.api.getAllBuckets(this.current_page.toString(), this.per_page.toString(), this.search)
+      .subscribe(response => {
+        this.bucketlists = response;
+      });
+    }
   }
 
+  showModal(){
+    $('.solvemodal').prependTo("body");
+  }
+
+  addBucket(bucket: Bucket) {
+    this.api.createBucket(this.newBucket)
+    .subscribe(response => {
+      this.bucketlists = this.bucketlists.concat(response);
+    });
+  }
+
+  deleteBucket(bucket: Bucket) {
+    this.api.deleteBucket(bucket)
+    .subscribe(response => {
+      this.bucketlists = this.bucketlists.filter((b) => b.id !== bucket.id);
+    });
+  }
+
+  nextPage() {
+    this.api.getAllBuckets(this.next.toString(), this.per_page.toString(), this.search)
+    .subscribe(response => {
+      this.bucketlists = response;
+    });
+    this.temp = this.next;
+    this.current_page = this.temp;
+    this.prev = this.current_page - 1;
+    this.next = this.current_page + 1;
+  }
+
+  prevPage() {
+    this.api.getAllBuckets(this.prev.toString(), this.per_page.toString(), this.search)
+    .subscribe(response => {
+      this.bucketlists = response;
+    });
+    this.temp = this.prev;
+    this.current_page = this.temp;
+    this.prev = this.current_page - 1;
+    this.next = this.current_page + 1;
+  }
+
+  // searchBuckets(search: string) {
+  //   this.bucketlists = this.bucketlists.filter((b) => b.name.indexOf(search) !== -1);
+  // }
+  searchBuckets(search: string) {
+    this.api.getAllBuckets(this.current_page.toString(), this.per_page.toString(), search)
+    .subscribe(response => {
+      this.bucketlists = response;
+    });
+  }
 }
